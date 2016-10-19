d05f2c383231c530f565f1c8fbccd91a70a537b7
Singirikonda <singirikondas@b2b.regn.net>
2016-10-19 09:33:01 -0500

    added-subs-tests

---
+++ src/main/java/SubsidiaryList.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class SubsidiaryList extends KYCTestBase {
+
+    @Test
+    public void verifyEntityDetailsPage(){
+        openWebApp("");
+        login("sunilubo");
+        openWebApp("https://internal-uboqa-web-1999720210.us-east-1.elb.amazonaws.com/kyc-webapp/#/legalEntity/3/ownership/subsidiaries/summary");
+        //dumpPage("Subsidiaries","specs/subsidiaries.gspec","subsidiaries");
+        checkPageLayout("specs/subsidiaries.gspec");
+    }
+
+}
