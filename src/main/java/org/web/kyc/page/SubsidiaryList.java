0148dca52d4c276ba868f61508f1e788e1dd3808
Singirikonda <singirikondas@b2b.regn.net>
2016-10-27 16:42:28 -0500

    updated-subs-list-loop

--- src/main/java/org/web/kyc/page/SubsidiaryList.java
+++ src/main/java/org/web/kyc/page/SubsidiaryList.java
@@ -4,13 +4,17 @@ import org.testng.annotations.Test;
 
 public class SubsidiaryList extends KYCTestBase {
 
+    public String subsSummary = "https://internal-uboqa-web-1999720210.us-east-1.elb.amazonaws.com/kyc-webapp/#/legalEntity/3/ownership/subsidiaries/summary";
+
     @Test
     public void verifySusbsidiarySummaryPage(){
         openWebApp("");
-        login("sunilubo");
-        openWebApp("https://internal-uboqa-web-1999720210.us-east-1.elb.amazonaws.com/kyc-webapp/#/legalEntity/3/ownership/subsidiaries/summary");
+        login(readProperties.getUboUser());
+        openWebApp(subsSummary);
+        getDriver().manage().window().maximize();
+
         //dumpPage("Subsidiaries","specs/subsidiaries.gspec","subsidiaries");
-        checkPageLayout("specs/subsidiaries.gspec");
+      checkPageLayout("specs/subsidiaries.gspec");
     }
 
 }
