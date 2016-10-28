72490d18f3477d7b25ed63de740773a3ba6fb36a
shahc1 <chintan.shah@accuity.com>
2016-10-28 08:59:27 -0500

    owners summary

--- src/main/java/org/web/kyc/page/OwnersSummary.java
+++ src/main/java/org/web/kyc/page/OwnersSummary.java
@@ -8,14 +8,14 @@ import org.testng.annotations.Test;
  */
 public class OwnersSummary extends KYCTestBase {
 
-    public String ownersSummary = "https://internal-uboqa-web-1999720210.us-east-1.elb.amazonaws.com/kyc-webapp/#/legalEntity/1038/ownership/owners/summary";
+    public String ownersSummary = "https://internal-uboqa-web-1999720210.us-east-1.elb.amazonaws.com/kyc-webapp/#/legalEntity/LE-A/ownership/owners/summary";
     @Test
     public void verifyOwnersSummaryPage(){
 
         openWebApp("");
         login(readProperties.getUboUser());
         openWebApp(ownersSummary);
-       // dumpPage("Owners Summary","specs/ownersSummary.gspec","ownersSummary");
+//        dumpPage("Owners Summary","specs/ownersSummary.gspec","ownersSummary");
         checkPageLayout("specs/ownersSummary.gspec");
     }
 }
