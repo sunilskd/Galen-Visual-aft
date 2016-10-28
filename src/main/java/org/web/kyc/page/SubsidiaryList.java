e7bb91ab6d56b59f1ea26a3b26ad9042b21e5f31
Singirikonda <singirikondas@b2b.regn.net>
2016-10-28 16:38:21 -0500

    updated-subslist-logic

--- src/main/java/org/web/kyc/page/SubsidiaryList.java
+++ src/main/java/org/web/kyc/page/SubsidiaryList.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class SubsidiaryList extends KYCTestBase {
 
-    public String subsSummary = "https://internal-uboqa-web-1999720210.us-east-1.elb.amazonaws.com/kyc-webapp/#/legalEntity/3/ownership/subsidiaries/summary";
+    public String subsSummary = "https://internal-uboqa-web-1999720210.us-east-1.elb.amazonaws.com/kyc-webapp/#/legalEntity/211/ownership/subsidiaries/summary";
 
     @Test
     public void verifySusbsidiarySummaryPage(){
