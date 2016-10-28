cce5830365886be0fc60371da2dc8eff6f45f952
shahc1 <chintan.shah@accuity.com>
2016-10-28 16:47:08 -0500

    updated review comments

--- src/main/java/org/web/kyc/page/DumpPage.java
+++ src/main/java/org/web/kyc/page/DumpPage.java
@@ -4,21 +4,21 @@ import org.testng.annotations.Test;
 
 public class DumpPage extends KYCTestBase {
 
-    @Test
-    public void dumpEntityDetailsPage(){
-        openWebApp("");
-        login(readProperties.getUboUser());
-        openWebApp(readProperties.getUrl() + "/#/legalEntity/58285/entityDetails");
-        dumpPage("Entity Details","specs/entitydetails.gspec","entitydetails");
-    }
-
-    @Test
-    public void dumpOwnersGraphPage(){
-        openWebApp("");
-        login(readProperties.getUboUser());
-        openWebApp(readProperties.getUrl() + "/#/legalEntity/11262/ownership/owners/graph");
-        dumpPage("Owners Graph","specs/graphs.gspec","ownersgraph");
-    }
+//    @Test
+//    public void dumpEntityDetailsPage(){
+//        openWebApp("");
+//        login(readProperties.getUboUser());
+//        openWebApp(readProperties.getUrl() + "/#/legalEntity/58285/entityDetails");
+//        dumpPage("Entity Details","specs/entitydetails.gspec","entitydetails");
+//    }
+//
+//    @Test
+//    public void dumpOwnersGraphPage(){
+//        openWebApp("");
+//        login(readProperties.getUboUser());
+//        openWebApp(readProperties.getUrl() + "/#/legalEntity/11262/ownership/owners/graph");
+//        dumpPage("Owners Graph","specs/graphs.gspec","ownersgraph");
+//    }
 
     @Test
     public void dumpOwnersSummaryPage(){
